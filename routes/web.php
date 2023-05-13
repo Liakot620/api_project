918ee6b87fb9e4442098cdce9f41399a78f2152b
MacBook Air <macbookair@MacBook-Pro-alkhas-b-MacBook.local>
2023-05-13 21:16:06 +0600

    first commit

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/students',function(){
+//     return 'web all requent';
+// });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
